534d58c63544d3502e2de7457e6dd3eafedec910
Arnaud Ligny <arnaud@ligny.org>
2017-03-19 00:15:33 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,12 +12,12 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-echo "Starting to update gh-pages..."
+echo "Starting to deploy to GitHub Pages (in '$TARGET_BRANCH' branch)"
 
 cp -R $SITE_DIR $HOME/$SITE_DIR
 cd $HOME
 git config --global user.name "Travis"
-git config --global user.email "contact@travis-ci.org"
+git config --global user.email "deploy@travis-ci.org"
 git clone --quiet --branch=$TARGET_BRANCH https://${GH_TOKEN}@github.com/${REPO}.git gh-pages > /dev/null
 cp -R gh-pages/.git $HOME/.git
 rm -rf gh-pages/*
@@ -25,5 +25,5 @@ cp -R $HOME/.git gh-pages/.git
 cd gh-pages
 cp -Rf $HOME/$SITE_DIR/* .
 git add -Af .
-git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to GitHub Pages"
 git push -fq origin $TARGET_BRANCH > /dev/null
