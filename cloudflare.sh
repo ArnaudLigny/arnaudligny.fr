26b64db70b12a607373c1e6f6ec31af3d5a2c1a7
Arnaud Ligny <arnaud@ligny.org>
2017-03-18 23:56:51 +0100

    Create cloudflare.sh

---
+++ cloudflare.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -X DELETE \"https://api.cloudflare.com/client/v4/zones/${Cloudflare_zone}/purge_cache\" \
+    -H \"X-Auth-Email: arnaud@ligny.org\" \
+    -H \"X-Auth-Key: ${Cloudflare_API}\" \
+    -H \"Content-Type: application/json\" \
+    --data '{\"purge_everything\":true}'
