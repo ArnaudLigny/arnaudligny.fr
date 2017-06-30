edca2ea42d135a4224202a6e2f805cf2b347efe9
Arnaud Ligny <arnaud@ligny.org>
2017-06-30 14:10:36 +0200

    Create netlify.sh

---
+++ netlify.sh
@@ -0,0 +1,12 @@
+echo "Switching to PHP 5.6.30"
+echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
+source ~/.phpbrew/bashrc
+phpbrew switch 5.6.30
+
+echo "Downloading PHPoole"
+curl -sSOL https://phpoole.org/phpoole.phar
+
+echo "Running PHPoole"
+php phpoole.phar build
+
+exit 0
