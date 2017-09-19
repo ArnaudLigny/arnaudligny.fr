7a82e6020e9e6ca8ed36ed94caacd1df4d0bfeae
Arnaud Ligny <arnaud@ligny.org>
2017-09-20 01:56:01 +0200

    Update netlify.sh

--- netlify.sh
+++ netlify.sh
@@ -7,7 +7,7 @@ echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 
 echo "Running PHPoole"
-php phpoole.phar version
+php phpoole.phar --version
 php phpoole.phar build
 
 exit 0
