38539dcc2fea3e6147e188715b364e6e86c63f14
Arnaud Ligny <arnaud@ligny.org>
2017-12-05 01:05:22 +0100

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -5,14 +5,9 @@ phpbrew switch 5.6.31
 
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
-# use lib
-#phpbrew app get composer
-#composer install
 
 echo "Running PHPoole"
 php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
-# use script
-#php scripts/build.php -e=prod
 
 exit 0
