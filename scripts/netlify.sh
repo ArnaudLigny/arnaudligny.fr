122a1afb60977c04bf19f36fb8c65f753fdbfacb
Arnaud Ligny <arnaud@ligny.org>
2018-02-21 18:20:12 +0100

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,14 +1,6 @@
-echo "Switching to PHP 5.6"
-echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
-source ~/.phpbrew/bashrc
-#phpbrew list
-phpbrew switch 5.6.33
-
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
-
-echo "Running PHPoole"
-php phpoole.phar --version
+echo "Using PHPoole" && php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 
 exit 0
