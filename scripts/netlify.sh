3a6d982957c1d1563be75e38f68ceb1704d069e4
Arnaud Ligny <arnaud@ligny.org>
2018-02-21 19:44:08 +0100

    Try new PHP support (#40)
    * Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,13 +1,5 @@
-echo "Switching to PHP 5.6"
-echo "[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc" >> ~/.bashrc
-source ~/.phpbrew/bashrc
-#phpbrew list
-phpbrew switch 5.6.33
-
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
-
-echo "Running PHPoole"
 php phpoole.phar --version
 if [ -z "$1" ]; then php phpoole.phar build; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 
