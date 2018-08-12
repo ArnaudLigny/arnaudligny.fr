8b81e586cc43b317a95aa3298252a6498d4d7ae4
Arnaud Ligny <arnaud@ligny.org>
2018-08-13 00:14:00 +0200

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -3,6 +3,6 @@
 echo "Downloading PHPoole"
 curl -sSOL https://phpoole.org/phpoole.phar
 php phpoole.phar --version
-if [ -z "$1" ]; then php phpoole.phar build -q; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
+if [ -z "$1" ]; then php phpoole.phar build --quiet; else echo "URL: $1" && php phpoole.phar build --baseurl=$1 --drafts; fi
 
 exit 0
