e06cde05ae5096cef648de0e0bffbe2cab6c6134
Arnaud Ligny <arnaud@ligny.org>
2019-03-03 17:24:13 +0100

    Update conf for beta test

---
+++ scripts/netlify-beta.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+echo "Downloading Cecil"
+curl -sSOL https://www.dropbox.com/s/tn0240fh4axpdg7/cecil-4.5.0-beta.phar?dl=1
+php cecil.phar --version
+
+echo "Started Cecil build"
+if [ -z "$1" ]; then php cecil.phar build --quiet; else echo "URL: $1" && php cecil.phar build --baseurl=$1 --drafts; fi
+echo "Finished Cecil build"
+
+exit 0
