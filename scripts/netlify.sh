c0ec320c03108cf753b03348c4181edf92bc5ce2
Arnaud Ligny <arnaud@ligny.org>
2019-05-08 23:43:26 +0200

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -4,11 +4,12 @@ echo "Downloading Cecil"
 curl -sSOL https://cecil.app/cecil.phar
 php cecil.phar --version
 
-echo "Add PHP Intl extensions"
+echo "Enabling PHP Intl extension"
 php -d extension=intl
 
 echo "Started Cecil build"
 if [ -z "$1" ]; then php cecil.phar build --quiet; else echo "URL: $1" && php cecil.phar build --verbose --baseurl=$1 --drafts; fi
+
 # build success? can deploy?
 if [ $? = 0 ]
 then
