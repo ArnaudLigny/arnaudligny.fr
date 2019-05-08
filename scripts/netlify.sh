03cb845d7092ccfdbb9c73b6ad4af2725c027918
Arnaud Ligny <arnaud@ligny.org>
2019-05-08 23:52:39 +0200

    Update netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -11,11 +11,6 @@ echo "Started Cecil build"
 if [ -z "$1" ]; then php cecil.phar build --quiet; else echo "URL: $1" && php cecil.phar build --verbose --baseurl=$1 --drafts || { sleep 120; false; }; fi
 
 # build success? can deploy?
-if [ $? = 0 ]
-then
-  echo "Finished Cecil build"
-  exit 0
-fi
+if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
 
-echo "Interrupted Cecil build"
-exit 1
+echo "Interrupted Cecil build"; exit 1
