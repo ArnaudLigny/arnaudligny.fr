2419342e5276ac9caf3f102ea274178a16b5c3b3
Arnaud Ligny <arnaud@ligny.org>
2019-05-09 00:02:03 +0200

    Intl fail... (#57)

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -4,8 +4,13 @@ echo "Downloading Cecil"
 curl -sSOL https://cecil.app/cecil.phar
 php cecil.phar --version
 
-echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --verbose; else echo "URL: $1" && php cecil.phar build --baseurl=$1 --drafts; fi
-echo "Finished Cecil build"
+echo "Enabling PHP Intl extension"
+php -d extension=intl
 
-exit 0
+echo "Started Cecil build"
+if [ -z "$1" ]; then php cecil.phar build --quiet; else echo "URL: $1" && php cecil.phar build --verbose --baseurl=$1 --drafts || { sleep 30; false; }; fi
+
+# build success? can deploy?
+if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
+
+echo "Interrupted Cecil build"; exit 1
