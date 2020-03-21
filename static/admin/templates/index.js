5256fcb1596315f012277f1109d722d3f92e1a13
Arnaud Ligny <arnaud@ligny.org>
2020-03-21 01:45:09 +0100

    Update index.js

--- static/admin/templates/index.js
+++ static/admin/templates/index.js
@@ -1,4 +1,6 @@
 import Post from '/admin/templates/post.js'
 
 CMS.registerPreviewStyle('/css/style.css');
+CMS.registerPreviewStyle('/styles.min.css');
+
 CMS.registerPreviewTemplate('billets', PostPreview);
