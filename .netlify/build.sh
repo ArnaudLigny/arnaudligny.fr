11d4861c6ecec9b2ea46b97ffa6715a834f78372
Arnaud Ligny <arnaud@ligny.org>
2020-04-01 23:13:56 +0200

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -e
-
 echo "Downloading Cecil"
 curl -sSOL https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
 php cecil.phar --version
 
 echo "Started Cecil build"
-if [ -z "$1" ]; then php cecil.phar build --quiet --postprocess; else echo "URL: $1" && php cecil.phar build -v --baseurl=$1 --drafts || { sleep 30; false; }; fi
+if [[ $1 == "preview" ]]; then
+  php cecil.phar build -v --baseurl=$DEPLOY_PRIME_URL --drafts || { sleep 30; false; }
+else
+  php cecil.phar build -v --baseurl=$URL --postprocess
+fi
 
 # build success? can deploy?
 if [ $? = 0 ]; then echo "Finished Cecil build"; exit 0; fi
