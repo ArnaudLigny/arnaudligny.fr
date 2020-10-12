e3dd306774660c16ae2056532db2a6e344bafa1f
Arnaud Ligny <arnaud@ligny.org>
2020-10-12 22:12:21 +0200

    Update index.js

--- static/admin/templates/index.js
+++ static/admin/templates/index.js
@@ -1,4 +1,4 @@
-import Post from '/admin/templates/post.js'
+//import Post from '/admin/templates/post.js'
 
-CMS.registerPreviewStyle('/css/garth.e83f873d735b7b1e43d9c47ecd5e9140.min.css');
-CMS.registerPreviewTemplate('billets', Post);
+//CMS.registerPreviewStyle('/css/garth.e83f873d735b7b1e43d9c47ecd5e9140.min.css');
+//CMS.registerPreviewTemplate('billets', Post);
