e6935721eaa988ff33445e12f7393db00995bf30
Arnaud Ligny <arnaud@ligny.org>
2020-10-12 19:17:19 +0200

    Update index.js

--- static/admin/templates/index.js
+++ static/admin/templates/index.js
@@ -1,7 +1,4 @@
-//import CMS from 'netlify-cms-app';
-//import { fr } from 'netlify-cms-locales';
 import Post from '/admin/templates/post.js'
 
-CMS.registerLocale('fr', fr);
 CMS.registerPreviewStyle('/styles.min.css');
 CMS.registerPreviewTemplate('billets', Post);
