ddc5d01c21f0bac753a6a07ed53be724376134c5
Arnaud Ligny <arnaud@ligny.org>
2020-11-06 22:40:08 +0100

    Update build.sh

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,5 +1,9 @@
 echo "Downloading Cecil"
-curl -sSOL https://github.com/Cecilapp/Cecil/releases/latest/download/cecil.phar
+if [ -z $CECIL_VERSION ]; then
+  curl -sSOL https://cecil.app/cecil.phar
+else
+  curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
+fi
 php cecil.phar --version
 
 echo "Started Cecil build"
