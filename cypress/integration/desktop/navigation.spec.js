c5beffff6e00885048c6a4be483c7be801cdf553
Arnaud Ligny <arnaud@ligny.org>
2021-02-02 22:39:43 +0100

    Update specs

---
+++ cypress/integration/desktop/navigation.spec.js
@@ -0,0 +1,11 @@
+describe('Navigation', function () {
+  it('Blog post, about, home', function () {
+    cy.viewport(1024, 768)
+    cy.visit('/')
+    cy.get('.container > .nav > .list > .item--nav-2 > a').click()
+    cy.visit('/blog/')
+    cy.get('.list > .item:nth-child(1) > .article > h2 > a').click()
+    cy.get('.footer > .container > .copyright > .small > a:nth-child(3)').click()
+    cy.get('body > .header > .container > .logo > img').click()
+  })
+})
