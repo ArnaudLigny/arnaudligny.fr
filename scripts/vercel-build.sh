5588c778a3a1db0423f6131a79aca55f306762db
Arnaud Ligny <arnaud@ligny.org>
2021-12-06 16:21:43 +0100

    Update vercel-build.sh

--- scripts/vercel-build.sh
+++ scripts/vercel-build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 echo "================================================================================"
-# prod
-if [ -z $VERCEL_URL ]; then
-  php cecil.phar build -v --config=config_prod.yml
-# preview
+if [[ $VERCEL_ENV == "production" ]]; then
+  if [ -z $VERCEL_URL ]; then
+    php cecil.phar build -v --config=config_prod.yml --postprocess
+  else
+    php cecil.phar build -v --baseurl=https://$VERCEL_URL --config=config_prod.yml --postprocess
+    echo "URL: https://$VERCEL_URL"
+  fi
 else
-  php cecil.phar build -vv --baseurl=https://$VERCEL_URL
-  echo "URL: https://$VERCEL_URL"
+  if [ -z $VERCEL_URL ]; then
+    php cecil.phar build -v --drafts
+  else
+    php cecil.phar build -v --baseurl=https://$VERCEL_URL --drafts
+    echo "URL: https://$VERCEL_URL"
+  fi
 fi
 
 # build success? can deploy?
