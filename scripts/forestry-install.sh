60a626f6761978002998ba03d8da040deed23bcc
Arnaud Ligny <arnaud@ligny.fr>
2021-12-30 00:56:26 +0100

    Update forestry-install.sh

--- scripts/forestry-install.sh
+++ scripts/forestry-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+echo "Downloading Cecil..."
 if [ -z $CECIL_VERSION ]; then
-  echo "Downloading Cecil..."
   curl -sSOL https://cecil.app/cecil.phar
 else
-  echo "Downloading Cecil ${CECIL_VERSION}..."
   curl -sSOL https://cecil.app/download/$CECIL_VERSION/cecil.phar
 fi
+php cecil.phar --version
 
 if [ -f "./composer.json" ]; then
   echo "Installing themes..."
