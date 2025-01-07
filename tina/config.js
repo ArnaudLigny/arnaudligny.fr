9c69064f9257c6e80d13eebe0f6272759004aaac
Arnaud Ligny <arnaud@ligny.fr>
2025-01-07 01:32:55 +0100

    Test

---
+++ tina/config.js
@@ -0,0 +1,55 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "master";
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  //clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: '0d3ef46a-7bfa-4814-b07a-b4c839d1487a',
+  // Get this from tina.io
+  //token: process.env.TINA_TOKEN,
+  token: '0d0c31719dcaac8d74a9f9f64433db0adce06f59',
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "pages/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
